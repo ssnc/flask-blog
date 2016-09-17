e2c6041696d5604780c392de6d049f2a287fb4bc
ssnc <wt29917@163.com>
2016-09-17 22:40:47 +0800

    first upload

--- practise.py
+++ practise.py
@@ -5,7 +5,7 @@ from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
 render_template, flash
 
-
+####
 app = Flask(__name__)
 
 '''#url根路由
